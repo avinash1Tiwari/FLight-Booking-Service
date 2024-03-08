2ffd969932956353d1f7234ddfa76df3f79abff7
avinash <avinashtiwari2405@gmail.com>
2024-03-09 00:33:36 +0530

    added booking models alongwith utils from flightService

--- src/utils/common/Enum.js
+++ src/utils/common/Enum.js
@@ -1,10 +1,20 @@
-SEAT_TYPE = {
+const SEAT_TYPE = {
     BUSSINESS:'bussiness',
     ECONOMY : 'economy',
     PREMIUM_ECONOMY : 'premium-economy',
     FIRST_CLASS : 'first_class'
 }
 
+
+const BOOKING_STATUS = {
+    BOOKED : 'booked',
+    CANCELLED : 'cancelled',
+    INITIATED : 'initiated',
+    PENDING : 'pending'
+}
+
+
 module.exports = {
-    SEAT_TYPE
+    SEAT_TYPE,
+    BOOKING_STATUS
 }
