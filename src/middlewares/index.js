dacd48f3a58ad3899813ea747c772a7586799bf4
avinash <avinashtiwari2405@gmail.com>
2024-03-17 16:56:44 +0530

    all things are now up to date to start implementing booking services stuffs

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,4 +1,3 @@
 module.exports = {
 
-    AirplaneMiddlewares : require('./airplane-middleware')
 }
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,11 +2,9 @@ const express = require('express');
 
 const { InfoController } = require('../../controllers');
 
-const airplaneRoutes = require('./airplane-routes')
 
 const router = express.Router();
 
-router.use('/airplanes',airplaneRoutes);
 
 router.get('/info', InfoController.info);
 
